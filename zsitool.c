a3cc14a3f985da489b216154ed1c5e2874ea8d32
Tomasz Mon <desowin@gmail.com>
2011-10-14 14:47:04 +0200

    add COPYING file

--- zsitool.c
+++ zsitool.c
@@ -57,7 +57,7 @@ int main(int argc, char **argv)
 
     if (argc < 2)
     {
-        printf("Usage: ./connecttool file1.srr file2.srr\n");
+        printf("Usage: ./zsitool file1.srr file2.srr\n");
         return 1;
     }
 
