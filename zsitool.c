40914f5a08062af1e7aaab79a8af68c77be94d59
Tomasz Moń <desowin@gmail.com>
2016-05-19 15:58:43 +0300

    Add bootloader_transfer_files()

--- zsitool.c
+++ zsitool.c
@@ -22,15 +22,7 @@
 
 #define TIMEOUT 1000
 
-static struct libusb_device_handle *handle = NULL;
-
-static int open_sansa_connect()
-{
-    handle = libusb_open_device_with_vid_pid(NULL, 0x0781, 0x7481);
-    return handle ? 0 : -EIO;
-}
-
-static int write_data(int endpoint, unsigned char *buf, int len)
+static int write_data(struct libusb_device_handle *handle, int endpoint, unsigned char *buf, int len)
 {
     int val;
     int i;
@@ -50,6 +42,105 @@ static int write_data(int endpoint, unsigned char *buf, int len)
         len -= i;
     }
     return val;
+}
+
+static bool bootloader_transfer_files(char **filenames, int n_files)
+{
+    struct libusb_device_handle *handle;
+    int file_nr;
+    int result;
+    unsigned char buf[4096];
+
+    result = libusb_init(NULL);
+    if (result < 0)
+    {
+        fprintf(stderr, "Failed to initialise libusb\n");
+        return 1;
+    }
+
+    handle = libusb_open_device_with_vid_pid(NULL, 0x0781, 0x7481);
+    if (!handle)
+    {
+        fprintf(stderr, "Unable to open Sansa Connect\n");
+        libusb_exit(NULL);
+        return false;
+    }
+
+    result = libusb_claim_interface(handle, 0);
+    if (result != 0)
+    {
+        printf("claim resulted with %d\n", result);
+        libusb_close(handle);
+        libusb_exit(NULL);
+        return false;
+    }
+
+    for (file_nr = 0; file_nr < n_files; file_nr++)
+    {
+        FILE *f = fopen(filenames[file_nr], "r");
+        if (f == NULL)
+        {
+            fprintf(stderr, "Unable to open file %s\n", filenames[file_nr]);
+            libusb_close(handle);
+            libusb_exit(NULL);
+            return false;
+        }
+        printf("Opening %s\n", filenames[file_nr]);
+
+        /* Read header */
+        fread(buf, sizeof(char), 16, f);
+
+        /* Check magic */
+        if (buf[0] != 0xAA || buf[1] != 0xBB ||
+            buf[2] != 0xFF || buf[3] != 0xEE)
+        {
+            fprintf(stderr, "File %s is not valid .srr file\n",
+                    filenames[file_nr]);
+            fclose(f);
+            continue;
+        }
+
+        /* Send header to device */
+        libusb_control_transfer(handle,
+                                0x40, 0x00, 0xEEEE, 0xEEEE,
+                                buf, 16, TIMEOUT);
+
+        /* Send data to device */
+        while (!feof(f))
+        {
+            size_t len;
+
+            len = fread(buf, sizeof(char), sizeof(buf), f);
+
+            if (write_data(handle, 1, buf, len) != 0)
+            {
+                fclose(f);
+                libusb_close(handle);
+                libusb_exit(NULL);
+                return false;
+            }
+
+            printf(".");
+            fflush(stdout);
+        }
+        printf("Done\n");
+        fclose(f);
+    }
+
+    /* Notify device that all files are sent */
+    libusb_control_transfer(handle,
+                            0x40, 0x00, 0xFFFF, 0xFFFF,
+                            NULL, 0, TIMEOUT);
+
+    result = libusb_release_interface(handle, 0);
+    if (result < 0)
+    {
+        printf("unable to release interface!\n");
+    }
+
+    libusb_close(handle);
+    libusb_exit(NULL);
+    return true;
 }
 
 static bool check_file(const char *filename)
@@ -87,9 +178,6 @@ static bool check_file(const char *filename)
 
 int main(int argc, char **argv)
 {
-    int result;
-    unsigned char buf[4096];
-
     if (argc == 2)
     {
         if (!check_file(argv[1]))
@@ -105,92 +193,11 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    result = libusb_init(NULL);
-    if (result < 0)
-    {
-        fprintf(stderr, "Failed to initialise libusb\n");
-        return 1;
-    }
-
-    if (open_sansa_connect() != 0)
-    {
-        fprintf(stderr, "Unable to open Sansa Connect\n");
-        libusb_exit(NULL);
-        return 1;
-    }
-
-    result = libusb_claim_interface(handle, 0);
-    if (result == 0)
-        printf("claim resulted with %d\n", result);
-    int file_nr;
-
-    /* TODO: do commandline arguments parsing */
-    int files = argc -1;
+    int files = argc - 1;
     char **filenames = &argv[1];
-
-    for (file_nr = 0; file_nr < files; file_nr++)
+    if (!bootloader_transfer_files(filenames, files))
     {
-        FILE *f = fopen(filenames[file_nr], "r");
-        if (f == NULL)
-        {
-            fprintf(stderr, "Unable to open file %s\n", filenames[file_nr]);
-            return 1;
-        }
-        printf("Opening %s\n", filenames[file_nr]);
-
-        /* Read header */
-        fread(buf, sizeof(char), 16, f);
-
-        /* Check magic */
-        if (buf[0] != 0xAA || buf[1] != 0xBB ||
-            buf[2] != 0xFF || buf[3] != 0xEE)
-        {
-            fprintf(stderr, "File %s is not valid .srr file\n",
-                    filenames[file_nr]);
-            fclose(f);
-            continue;
-        }
-
-        /* Send header to device */
-        libusb_control_transfer(handle,
-                                0x40, 0x00, 0xEEEE, 0xEEEE,
-                                buf, 16, TIMEOUT);
-
-        /* Send data to device */
-        while (!feof(f))
-        {
-            size_t len;
-
-            len = fread(buf, sizeof(char), sizeof(buf), f);
-
-            if (write_data(1, buf, len) != 0)
-            {
-                fclose(f);
-                libusb_close(handle);
-                libusb_exit(NULL);
-                return 1;
-            }
-
-            printf(".");
-            fflush(stdout);
-        }
-        printf("Done\n");
-        fclose(f);
+        return 1;
     }
-
-    /* Notify device that all files are sent */
-    libusb_control_transfer(handle,
-                            0x40, 0x00, 0xFFFF, 0xFFFF,
-                            NULL, 0, TIMEOUT);
-
-    result = libusb_release_interface(handle, 0);
-    if (result < 0)
-    {
-        printf("unable to release interface!\n");
-    }
-
-    libusb_close(handle);
-    libusb_exit(NULL);
-
     return 0;
 }
